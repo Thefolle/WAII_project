784fee4e69ce844f97a58c36e7522a8889acec2e
Davide Calarco <s280174@studenti.polito.it>
2021-07-01 19:07:43 +0200

    Write down kafka consumers design choices

--- order_service/src/main/kotlin/it/polito/waii/order_service/controllers/OrderController.kt
+++ order_service/src/main/kotlin/it/polito/waii/order_service/controllers/OrderController.kt
@@ -24,6 +24,20 @@ class OrderController {
     // - no communication with the other services
     // - the return value must be provided by sending another message to the caller
 
+    /* Design
+
+    There are right but conflicting design patterns:
+    a) All listeners should subscribe to the same partition so as to preserve order of messages;
+    b) There should be one listener per partition, because:
+        1) If there are more listeners than partitions, (#listeners - #partitions) listeners are never triggered;
+        2) If there are less listeners than partitions, the same handler should be in charge of serving multiple partitions.
+
+    The design pattern 'a' fall backs to the point 'b1', so the final solution chosen is 'b'.
+    The problem of this solution arises when three actors A, B and C are involved in a transaction:
+        if A creates an order, B is this service and C reads the orders, then the response sent to C is not deterministic.
+        if A = C, the problem vanishes since A can wait for the creation of the new order.
+    */
+
 //    @SendTo("topic1")
     @KafkaListener(topics = ["topic1"], containerFactory = "orderDtoConcurrentKafkaListenerContainerFactory", topicPartitions = [TopicPartition(topic = "topic1", partitions =  ["0"])])
     fun createOrder(orderDto: OrderDto): Long {
